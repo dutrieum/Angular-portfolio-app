2cb92cb8e5d940380599ba2e9050080765135bf4
Mathilde <dutrieuxmathilde@gmail.com>
2021-10-28 16:39:50 +0200

    Use of the interface and first display of gallery

---
+++ src/app/models/image.model.ts
@@ -0,0 +1,17 @@
+import {Model} from "./interfaces/model.interface";
+
+export class ImageModel implements Model {
+  readonly url : string
+  readonly desc : string
+  readonly height : number
+  readonly width : number
+  readonly likes : number
+
+  constructor(input: Partial<ImageModel>) {
+    this.url = input.url || '';
+    this.desc = input.desc || '';
+    this.height = input.height || 150;
+    this.width = input.width || 150;
+    this.likes = input.likes || 0;
+  }
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as faker from "faker";
+import {ImageModel} from "../../models/image.model";
 
 @Component({
   selector: 'app-home-page',
@@ -7,7 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  dataPage : ImageModel[]
+
+  constructor() {
+    this.dataPage = [
+      new ImageModel ({
+        url: faker.image.imageUrl(140,140,undefined,true, true),
+        desc: 'Lorem ipsum',
+        height: 250,
+        width: 250,
+        likes: 4,
+      }),
+      new ImageModel ({
+        url: faker.image.imageUrl(140,140,undefined,true, true),
+        desc: 'Lorem ipsum',
+        height: 250,
+        width: 250,
+        likes: 0,
+      }),
+      new ImageModel ({
+        url: faker.image.imageUrl(140,140,undefined,true, true),
+        desc: 'Lorem ipsum ipsum ipsum ipsum ipsum',
+        height: 250,
+        width: 250,
+        likes: 2,
+      }),
+    ]
+  }
 
   ngOnInit(): void {
   }
